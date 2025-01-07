49a123778642b379f547c2a37855251a8e696020
Kevin Newton <kddnewton@meta.com>
2025-01-07 06:14:47 -0800

    Fix ft_utils macOS GitHub actions build
    Summary:
    Basically, automatic TLS and dynamic TLS cannot be mixed, because the destructor order is not guaranteed.
    
    This commit also adds a bunch of documentation to _test_weave.c. It also adds a note about this limitation to weave_api.md.
    
    Reviewed By: SonicField
    
    Differential Revision: D67873632
    
    fbshipit-source-id: 643e701effc3221e877a7202234e7515787f3b86

--- _test_weave.c
+++ _test_weave.c
@@ -5,13 +5,24 @@
 
 static MUTEX_TYPE destructor_mutex;
 
+/* We use these values a sentinels to compare against in the various destructors
+ * and check functions.
+ */
+static int value1 = 0x12345678;
+static int value2 = 0x87654321;
+static void* ptr1 = (void*)&value1;
+static void* ptr2 = (void*)&value2;
+
+/* These are state variables, changed when destructors are called
+ */
 static int destructor_called_1 = 0;
 static int destructor_called_2 = 0;
 static int tls_check_1 = 0;
 static int tls_check_2 = 0;
-static weave_local void* tls_1 = NULL;
-static weave_local void* tls_2 = NULL;
 
+/* Reset all of the state variables to the default state. This function is
+ * called between tests to ensure they do not impact each other.
+ */
 static PyObject* test_reset(
     PyObject* Py_UNUSED(self),
     PyObject* Py_UNUSED(args)) {
@@ -21,16 +32,18 @@ static PyObject* test_reset(
   destructor_called_2 = 0;
   tls_check_1 = 0;
   tls_check_2 = 0;
-  tls_1 = NULL;
-  tls_2 = NULL;
   MUTEX_UNLOCK(destructor_mutex);
   Py_END_ALLOW_THREADS;
   Py_RETURN_NONE;
 }
 
+/* The callback passed to _py_register_wvls_destructor that will be called when
+ * the thread exits with the pointer associated with the value of the key. It
+ * increments the counter for the number of times this was called.
+ */
 static void test_destructor_add_1(void* addr) {
   MUTEX_LOCK(destructor_mutex);
-  if (addr == tls_1) {
+  if (addr == ptr1) {
     destructor_called_1 += 1;
   } else {
     tls_check_1 = 1;
@@ -38,9 +51,14 @@ static void test_destructor_add_1(void* addr) {
   MUTEX_UNLOCK(destructor_mutex);
 }
 
+/* The callback passed to _py_register_wvls_destructor that will be called when
+ * the thread exits with the pointer associated with the value of the key.
+ * Importantly this sets the value to 100 instead of 1 to indicate that this
+ * callback was called instead.
+ */
 static void test_destructor_reset_1(void* addr) {
   MUTEX_LOCK(destructor_mutex);
-  if (addr == tls_1) {
+  if (addr == ptr1) {
     destructor_called_1 = 100;
   } else {
     tls_check_1 = 1;
@@ -48,9 +66,14 @@ static void test_destructor_reset_1(void* addr) {
   MUTEX_UNLOCK(destructor_mutex);
 }
 
+/* The callback passed to _py_register_wvls_destructor that will be called when
+ * the thread exits with the pointer associated with the value of the key. This
+ * is the second callback to be registered in the tests. It increments the
+ * second counter for the number of times this was called.
+ */
 static void test_destructor_add_2(void* addr) {
   MUTEX_LOCK(destructor_mutex);
-  if (addr == tls_2) {
+  if (addr == ptr2) {
     destructor_called_2 += 1;
   } else {
     tls_check_2 = 1;
@@ -58,6 +81,10 @@ static void test_destructor_add_2(void* addr) {
   MUTEX_UNLOCK(destructor_mutex);
 }
 
+/* Returns a Python integer that represents whether or not the destructor was
+ * properly called with the correct value. If it was not, an error is raised.
+ * Otherwise it returns the nubmer of times the destructor was called.
+ */
 static PyObject* test_weave_get_destructor_called_1(
     PyObject* Py_UNUSED(self),
     PyObject* Py_UNUSED(args)) {
@@ -78,6 +105,10 @@ static PyObject* test_weave_get_destructor_called_1(
   return PyLong_FromLong(c1);
 }
 
+/* Returns a Python integer that represents whether or not the second destructor
+ * was properly called with the correct value. If it was not, an error is
+ * raised. Otherwise it returns the nubmer of times the destructor was called.
+ */
 static PyObject* test_weave_get_destructor_called_2(
     PyObject* Py_UNUSED(self),
     PyObject* Py_UNUSED(args)) {
@@ -98,52 +129,62 @@ static PyObject* test_weave_get_destructor_called_2(
   return PyLong_FromLong(c2);
 }
 
+/* Register the first destructor with weave.
+ */
 static PyObject* test_weave_register_destructor_1(
     PyObject* Py_UNUSED(self),
     PyObject* Py_UNUSED(args)) {
-  int ret = _py_register_wvls_destructor(&tls_1, &test_destructor_add_1);
+  int ret = _py_register_wvls_destructor(&ptr1, &test_destructor_add_1);
   if (ret != 0) {
     return NULL;
   }
   Py_RETURN_NONE;
 }
 
+/* Register the second destructor with weave.
+ */
 static PyObject* test_weave_register_destructor_2(
     PyObject* Py_UNUSED(self),
     PyObject* Py_UNUSED(args)) {
-  int ret = _py_register_wvls_destructor(&tls_2, &test_destructor_add_2);
+  int ret = _py_register_wvls_destructor(&ptr2, &test_destructor_add_2);
   if (ret != 0) {
     return NULL;
   }
   Py_RETURN_NONE;
 }
 
+/* Register the first destructor that will set the "reset" value.
+ */
 static PyObject* test_weave_register_destructor_reset_1(
     PyObject* Py_UNUSED(self),
     PyObject* Py_UNUSED(args)) {
-  int ret = _py_register_wvls_destructor(&tls_1, &test_destructor_reset_1);
+  int ret = _py_register_wvls_destructor(&ptr1, &test_destructor_reset_1);
   if (ret != 0) {
     return NULL;
   }
   Py_RETURN_NONE;
 }
 
+/* Remove the first destructor by unregistering it with weave.
+ */
 static PyObject* test_weave_unregister_destructor_1(
     PyObject* Py_UNUSED(self),
     PyObject* Py_UNUSED(args)) {
   int unreg = 0;
-  int ret = _py_unregister_wvls_destructor(&tls_1, &unreg);
+  int ret = _py_unregister_wvls_destructor(&ptr1, &unreg);
   if (ret != 0) {
     return NULL;
   }
   return PyLong_FromLong(unreg);
 }
 
+/* Remove the second destructor by unregistering it with weave.
+ */
 static PyObject* test_weave_unregister_destructor_2(
     PyObject* Py_UNUSED(self),
     PyObject* Py_UNUSED(args)) {
   int unreg = 0;
-  int ret = _py_unregister_wvls_destructor(&tls_2, &unreg);
+  int ret = _py_unregister_wvls_destructor(&ptr2, &unreg);
   if (ret != 0) {
     return NULL;
   }
@@ -155,11 +196,11 @@ static PyMethodDef test_weave_module_methods[] = {
     {"register_destructor_1",
      test_weave_register_destructor_1,
      METH_NOARGS,
-     "Register a reset destructor for a tls 1"},
+     "Register a destructor for a tls 1"},
     {"register_destructor_reset_1",
      test_weave_register_destructor_reset_1,
      METH_NOARGS,
-     "Register a destructor for a tls 1"},
+     "Register a reset destructor for a tls 1"},
     {"unregister_destructor_1",
      test_weave_unregister_destructor_1,
      METH_NOARGS,
@@ -180,7 +221,8 @@ static PyMethodDef test_weave_module_methods[] = {
      test_weave_get_destructor_called_2,
      METH_NOARGS,
      "Get the value of destructor_called_2"},
-    {NULL, NULL, 0, NULL}};
+    {NULL, NULL, 0, NULL},
+};
 
 static int exec_test_weave_module(PyObject* module) {
   return MUTEX_INIT(destructor_mutex);
@@ -201,7 +243,8 @@ static PyModuleDef test_weave_module = {
     test_weave_module_slots,
     NULL,
     NULL,
-    NULL};
+    NULL,
+};
 
 PyMODINIT_FUNC PyInit__test_weave(void) {
   return PyModuleDef_Init(&test_weave_module);
