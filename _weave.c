00ee9131894d1c5974a618df7b07cc4d693f7378
Alex Turner <alexturner@meta.com>
2025-02-03 14:39:00 -0800

    ft_utils building for Windows 32-bit
    Summary:
    Previously, we were crashing on Windows 32-bit because the process was hanging on to the FLS callback (assumed, it was difficult to pinpoint).
    
    Now, instead of using FLS we are using TLS and hooking into DllMain to get notifications when the thread and process exit. We keep a linked list of requested TLS keys. The linked list is walked on thread detach to call all of the destructors in each of the threads. It is also walked on process exit to free all of the allocated keys.
    
    Doing it this was makes it work on 32-bit windows, as well as keeping our current capabilities. It also has the benefit of not leaking the FLS indices, which previously were not being freed.
    
    Reviewed By: czardoz
    
    Differential Revision: D68632202
    
    fbshipit-source-id: cab95f80cbbbd608c35408b81f82b986f1cc66ee

--- _weave.c
+++ _weave.c
@@ -9,105 +9,175 @@
 
 #ifdef _WIN32
 
-int wvls_ensure_fiber() {
-  if (!GetCurrentFiber()) {
-    if (!ConvertThreadToFiber(NULL)) {
-      return GetLastError();
+/* When dynamic TLS keys are created through wvls_key_create, we keep track of
+ * them in this linked list. On thread detach, we optionally call the callback
+ * associated with the key, then set the value to NULL. On process detach, we
+ * free the key and clear the linked list.
+ */
+typedef struct wvls_dynamic_key {
+  struct wvls_dynamic_key* next;
+  wvls_key_t key;
+  wvls_destructor_t destructor;
+} wvls_dynamic_key_t;
+
+/* This is a process-level linked-list that keeps track of the allocated TLS
+ * keys. On process detach, it will be walked to free all of the keys
+ */
+static wvls_dynamic_key_t* wvls_dynamic_keys = NULL;
+
+/* The DllMain entrypoint allows us to hook into the dll process to get notified
+ * when threads or processes are detached. Note that this works because the
+ * ft_utils native extension is loaded as a dynamic library.
+ *
+ * If, in the future, a situation arises where we need to load the native
+ * extension as a static library, there is some prior art we can copy from
+ * mimalloc that is the new suggested way to hook into these kinds of
+ * notifications here:
+ *
+ *   https://github.com/microsoft/mimalloc/blob/14b4f674fa2cff00f28333365fe07ce916575c30/src/prim/windows/prim.c#L658
+ */
+__declspec(dllexport) BOOL WINAPI
+DllMain(HINSTANCE hinst_dll, DWORD fdw_reason, LPVOID lp_reserved) {
+  /* If we have not been initialized yet, we do not need to worry about
+   * notifications coming from DllMain. */
+  if (!wvls_dynamic_keys) {
+    return TRUE;
+  }
+
+  switch (fdw_reason) {
+    case DLL_THREAD_DETACH: {
+      /* On thread detach, we want to visit each TLS key and call its callback
+       * if one has been registered and a value is associated with it. Then we
+       * want to set the value to NULL. Note that we do _not_ want to free the
+       * key here, since this needs to run on every thread being detached.
+       */
+      wvls_dynamic_key_t* dynamic_key = wvls_dynamic_keys;
+
+      while (dynamic_key) {
+        void* value = TlsGetValue(dynamic_key->key);
+        if (value) {
+          if (dynamic_key->destructor) {
+            dynamic_key->destructor(value);
+          }
+          TlsSetValue(dynamic_key->key, NULL);
+        }
+
+        dynamic_key = dynamic_key->next;
+      }
+
+      break;
+    }
+    case DLL_PROCESS_DETACH: {
+      /* On process detach, we want to visit each TLS key and return it, then
+       * we want to clear our own bookkeeping.
+       */
+      wvls_dynamic_key_t* dynamic_key = wvls_dynamic_keys;
+
+      while (dynamic_key) {
+        wvls_dynamic_key_t* next = dynamic_key->next;
+        TlsFree(dynamic_key->key);
+        free(dynamic_key);
+        dynamic_key = next;
+      }
+
+      wvls_dynamic_keys = NULL;
+      break;
     }
   }
-  return 0;
+
+  return TRUE;
 }
 
-void wvls_destructor_noop(void* Py_UNUSED(data)) {
-  /* PASS */
-}
-
+/* Create a weave key that will call the given destructor on thread exit. */
 int wvls_key_create(wvls_key_t* key, wvls_destructor_t destructor) {
   if (!key) {
     return ERROR_INVALID_PARAMETER;
   }
 
-  int ret_val = wvls_ensure_fiber();
-  if (ret_val) {
-    return ret_val;
-  }
-
-  if (!destructor) {
-    destructor = wvls_destructor_noop;
-  }
-
-  *key = FlsAlloc((PFLS_CALLBACK_FUNCTION)destructor);
-  if (*key == FLS_OUT_OF_INDEXES) {
+  /* First, create the requested dynamic key. */
+  *key = TlsAlloc();
+  if (*key == TLS_OUT_OF_INDEXES) {
     return GetLastError();
   }
+
+  /* Now keep track of the dynamic key. */
+  wvls_dynamic_key_t* dynamic_key =
+      (wvls_dynamic_key_t*)malloc(sizeof(wvls_dynamic_key_t));
+  if (!dynamic_key) {
+    TlsFree(*key);
+    return ENOMEM;
+  }
+
+  dynamic_key->next = wvls_dynamic_keys;
+  dynamic_key->key = *key;
+  dynamic_key->destructor = destructor;
+  wvls_dynamic_keys = dynamic_key;
 
   return 0;
 }
 
+/* Delete a weave key. Note that this will not call the callback associated with
+ * the key if one was given on creation. */
 int wvls_key_delete(wvls_key_t key) {
-  if (!key) {
+  if (!key || !wvls_dynamic_keys) {
     return ERROR_INVALID_PARAMETER;
   }
 
-  int ret_val = wvls_ensure_fiber();
-  if (ret_val) {
-    return ret_val;
+  /* Find the dynamic key in the list. */
+  wvls_dynamic_key_t* previous = NULL;
+  wvls_dynamic_key_t* current = wvls_dynamic_keys;
+
+  while (current != NULL && current->key != key) {
+    previous = current;
+    current = current->next;
   }
 
-  if (!FlsFree(key)) {
+  /* If we did not find the key, return an error. */
+  if (!current) {
+    return ERROR_INVALID_PARAMETER;
+  }
+
+  /* Remove the key from the list. */
+  if (previous) {
+    previous->next = current->next;
+  } else {
+    wvls_dynamic_keys = current->next;
+  }
+
+  /* Free the key. */
+  free(current);
+  if (!TlsFree(key)) {
     return GetLastError();
   }
 
   return 0;
 }
 
+/* Set the value associated with the given weave key. */
 int wvls_set_value(wvls_key_t key, void* value) {
   if (!key) {
     return ERROR_INVALID_PARAMETER;
   }
 
-  int ret_val = wvls_ensure_fiber();
-  if (ret_val) {
-    return ret_val;
-  }
-
-  /* If FlsGetValue returns NULL, it may be because the thread existed before
-     the DLL was loaded (e.g., main thread or threads created by other
-     libraries). In this case, we need to allocate memory for the TLS value and
-     set it. */
-  LPVOID lpvData = FlsGetValue(key);
-  if (lpvData == NULL && GetLastError() != ERROR_SUCCESS) {
-    lpvData = LocalAlloc(LPTR, 256);
-    if (lpvData == NULL) {
-      return ERROR_NOT_ENOUGH_MEMORY;
-    }
-    if (!FlsSetValue(key, lpvData)) {
-      LocalFree(lpvData);
-      return GetLastError();
-    }
-  }
-
-  if (!FlsSetValue(key, value)) {
+  if (!TlsSetValue(key, value)) {
     return GetLastError();
   }
 
   return 0;
 }
 
+/* Get the value associated with the given weave key. */
 void* wvls_get_value(wvls_key_t key) {
   if (!key) {
     return NULL;
   }
 
-  if (wvls_ensure_fiber()) {
-    return NULL;
-  }
-
-  return FlsGetValue(key);
+  return TlsGetValue(key);
 }
 
 #else /* POSIX */
 
+/* Create a weave key that will call the given callback on thread exit. */
 int wvls_key_create(wvls_key_t* key, wvls_destructor_t destructor) {
   if (!key) {
     return EINVAL;
@@ -116,6 +186,8 @@ int wvls_key_create(wvls_key_t* key, wvls_destructor_t destructor) {
   return pthread_key_create(key, destructor);
 }
 
+/* Delete a weave key. Note that this will not call the callback associated with
+ * the key if one was given on creation. */
 int wvls_key_delete(wvls_key_t key) {
   if (!key) {
     return EINVAL;
@@ -124,6 +196,7 @@ int wvls_key_delete(wvls_key_t key) {
   return pthread_key_delete(key);
 }
 
+/* Set the value associated with the given weave key. */
 int wvls_set_value(wvls_key_t key, void* value) {
   if (!key) {
     return EINVAL;
@@ -132,6 +205,7 @@ int wvls_set_value(wvls_key_t key, void* value) {
   return pthread_setspecific(key, value);
 }
 
+/* Get the value associated with the given weave key. */
 void* wvls_get_value(wvls_key_t key) {
   if (!key) {
     return NULL;
@@ -139,15 +213,16 @@ void* wvls_get_value(wvls_key_t key) {
 
   return pthread_getspecific(key);
 }
+
 #endif
+
+static wvls_key_t wvls_destructors_key;
 
 typedef struct wvls_destructor_node {
   void** wvls_variable_ptr;
   wvls_destructor_t destructor;
   struct wvls_destructor_node* next;
 } wvls_destructor_node_t;
-
-static wvls_key_t wvls_destructors_key;
 
 void wvls_destructors_invoke(void* arg) {
   wvls_destructor_node_t* node = (wvls_destructor_node_t*)arg;
@@ -176,12 +251,6 @@ static void init_wvls_destructor_key() {
   int oops = wvls_key_create(&wvls_destructors_key, wvls_destructors_invoke);
   if (oops) {
     fprintf(stderr, "Failed to create TLS key: %i.\n", oops);
-    abort();
-  }
-  /* Probably not necessary but let's be careful. */
-  oops = wvls_set_value(wvls_destructors_key, NULL);
-  if (oops) {
-    fprintf(stderr, "Failed to set TLS value : %i.\n", oops);
     abort();
   }
 }
